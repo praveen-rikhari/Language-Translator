6b39f2e0e642c8feeac11ceb342f2e61a1bad3bb
praveen-rikhari <praveenrikhari45@gmail.com>
2024-04-05 22:22:31 +0530

    Created App Layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,30 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1>Language Translator</h1>
+      <div className="App">
+        <h1>Language Translator</h1>
+        <div>
+          From :
+          <select>
+            <option value="1">1</option>
+            <option value="2">2</option>
+          </select>
+          To :
+          <select>
+            <option value="1">1</option>
+            <option value="2">2</option>
+          </select>
+        </div>
+        <div>
+          <textarea cols="50" rows="8"></textarea>
+        </div>
+        <div>
+          <textarea cols="50" rows="8"></textarea>
+        </div>
+        <div>
+          <button>Translate</button>
+        </div>
+      </div>
     </>
   )
 }
