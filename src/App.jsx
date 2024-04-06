443c890c0b7be743cb093f5a706456f4b1d31a6e
praveen-rikhari <praveenrikhari45@gmail.com>
2024-04-06 16:57:12 +0530

    GET request from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,40 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css'
 
 function App() {
+  const [to, setTo] = useState("en");
+  const [from, setFrom] = useState("en");
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
+  const [options, setOptions] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://libretranslate.com/languages',
+      {
+        headers: { 'accept': 'application/json' }
+      }).then(res => {
+        console.log(res)
+        setOptions(res.data);
+      });
+  }, [])
+
   return (
     <>
       <div className="App">
         <h1>Language Translator</h1>
         <div>
-          From :
-          <select>
-            <option value="1">1</option>
-            <option value="2">2</option>
+          From <b>{from}</b> :
+          <select onChange={(e) => setFrom(e.target.value)}>
+            {options.map((opt) => (
+              <option value={opt.code} key={opt.code} >{opt.name}</option>
+            ))}
           </select>
-          To :
-          <select>
-            <option value="1">1</option>
-            <option value="2">2</option>
+          To <b>{to}</b> :
+          <select onChange={(e) => setTo(e.target.value)}>
+            {options.map((opt) => (
+              <option value={opt.code} key={opt.code} >{opt.name}</option>
+            ))}
           </select>
         </div>
         <div>
